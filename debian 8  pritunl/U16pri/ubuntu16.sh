bd0d378ab4e74a607bd23f7e53d80965d578de0b
oi10536 <34789129+oi10536@users.noreply.github.com>
2018-04-02 10:53:13 +0700

    Add files via upload

---
+++ debian 8  pritunl/U16pri/ubuntu16.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# go to root
+cd
+apt-get update
+apt-get install nano
+apt-get install wget
+
+wget -O /etc/apt/sources.list "http://ptt101.hopto.org:81/kad78/de8u1416pritunl/u16/sources.list"
+
+wget http://ptt101.hopto.org:81/kad78/de8u1416pritunl/u16/install_pritunl16.sh && chmod +x install_pritunl16.sh && ./install_pritunl16.sh
+
+#!/bin/bash
+cd
+# Change to Time GMT+8
+ln -fs /usr/share/zoneinfo/Asia/Bangkok /etc/localtime
+
+#MYIP=$(wget -qO- ipv4.icanhazip.com);
+
+# Install Squid
+apt-get -y install squid
+systemctl start squid
+systemctl enable squid
+cp /etc/squid/squid.conf /etc/squid/squid.conf.orig
+wget -O /etc/squid/squid.conf "http://ptt101.hopto.org:81/kad78/de8u1416pritunl/u16/squid.conf" 
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
+sed -i s/xxxxxxxxx/$MYIP/g /etc/squid/squid.conf;
+systemctl restart squid
+clear
+
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+
+# Install Web Server
+apt-get -y install nginx php5-fpm php5-cli
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "http://ptt101.hopto.org:81/kad78/de8u1416pritunl/u14/nginx.conf"
+mkdir -p /home/vps/public_html
+echo "<pre>Setup by DRCYBER </pre>" > /home/vps/public_html/index.html
+echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
+wget -O /etc/nginx/conf.d/vps.conf "http://ptt101.hopto.org:81/kad78/de8u1416pritunl/u14/vps.conf"
+sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
+service php5-fpm restart
+service nginx restart
+
+# Install Vnstat
+apt-get -y install vnstat
+vnstat -u -i eth0
+sudo chown -R vnstat:vnstat /var/lib/vnstat
+service vnstat restart
+
+# Install Vnstat GUI
+cd /home/vps/public_html/
+wget http://ptt101.hopto.org:81/kad78/de8u1416pritunl/u14/vnstat_php_frontend-1.5.1.tar.gz
+tar xf vnstat_php_frontend-1.5.1.tar.gz
+rm vnstat_php_frontend-1.5.1.tar.gz
+mv vnstat_php_frontend-1.5.1 vnstat
+cd vnstat
+sed -i "s/\$iface_list = array('eth0', 'sixxs');/\$iface_list = array('eth0');/g" config.php
+sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+sed -i 's/Internal/Internet/g' config.php
+sed -i '/SixXS IPv6/d' config.php
+cd
+
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+
+cd
+# Enable Firewall
+sudo ufw allow 22,80,81,222,443,3128,8080/tcp
+sudo ufw allow 22,80,81,222,443,3128,8080/udp
+sudo yes | ufw enable
+#speed control vps
+apt-get install nano
+wget https://sangman.000webhostapp.com/bandwidth.sh
+chmod +x bandwidth.sh
+sed -i -e 's/\r$//' bandwidth.sh
+./bandwidth.sh
+#FIGlet In Linux
+apt-get install figlet
+# About
+clear
+figlet "Thai 4G"
+echo "Script mod by sangmander kasang"
+echo "-Pritunl"
+echo "-MongoDB"
+echo "-Squid Proxy Port 8080,3128"
+echo "Vnstat  :  http://$MYIP:81/vnstat"
+echo "Pritunl :  https://$MYIP"
+echo "Login pritunl?"
+echo "copy key"
+pritunl setup-key
